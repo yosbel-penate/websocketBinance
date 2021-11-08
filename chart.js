bfed4061c611f0ca0e07eca0fdf8bbbfb9fe5172
yosbel-penate <yosbel.penate@gmail.com>
2021-11-08 23:02:32 +0300

    +++

--- chart.js
+++ chart.js
@@ -1,9 +1,25 @@
 
-var chart = LightweightCharts.createChart(document.getElementById('chart'), {
+var chart = LightweightCharts.createChart(document.getElementById('chart'),  {
 	width: 600,
-    height: 300,
-	crosshair: {
-		mode: LightweightCharts.CrosshairMode.Normal,
+  height: 300,
+	layout: {
+		backgroundColor: '#ffffff',
+		textColor: 'rgba(33, 56, 77, 1)',
+	},
+  localization: {
+        locale: 'cn-CN',
+    },
+	grid: {
+		vertLines: {
+			color: 'rgba(197, 203, 206, 0.7)',
+		},
+		horzLines: {
+			color: 'rgba(197, 203, 206, 0.7)',
+		},
+	},
+	timeScale: {
+		timeVisible: true,
+    secondsVisible: false,
 	},
 });
 
@@ -22,12 +38,19 @@ var socket = new WebSocket('wss://stream.binance.com:9443/ws/btcusdt@kline_1m');
 socket.onmessage = function (event) {
     var message = JSON.parse(event.data)
     var candlestick = message.k;
-
-    entry.time=  candlestick.t;
+    
+    entry.time = humanDateFormat(candlestick.t);
     entry.open =  candlestick.o;
     entry.high = candlestick.h;
     entry.low = candlestick.l;
     entry.close = candlestick.c;
-
     candleSeries.update(entry);
 }
+
+function humanDateFormat(unixTimestamp)
+{
+    return dateToChartTimeMinute(new Date(unixTimestamp*1000));
+}
+function dateToChartTimeMinute(date) {
+	return Date.UTC(date.getFullYear(), date.getMonth(), date.getDate(), date.getHours(), date.getMinutes(), 0, 0) / 1000;
+};
